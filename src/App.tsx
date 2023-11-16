2980e5420f0895b2f1e45bbecd17c93b55b95426
marcelamejiao <mejiao24@hotmail.com>
2023-11-16 19:20:52 +1100

    implement routes inside App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import AddEmployeeForm from "./components/AddEmployeeForm/AddEmployeeForm";
+import EmployeeList from "../src/container/EmployeeList/EmployeeList";
+import Nav from "./components/NavBar/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <AddEmployeeForm />
-    </>
+    <BrowserRouter>
+    <Nav />
+      <Routes>
+      <Route
+          path="/"
+          element={
+            <EmployeeList />
+          }
+        />
+        <Route
+          path="/add-employee"
+          element={
+            <AddEmployeeForm />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
