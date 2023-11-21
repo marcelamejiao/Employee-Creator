f2c81f5196ba4ebd1a67bb9152d6e76a62c631ad
marcelamejiao <mejiao24@hotmail.com>
2023-11-21 15:39:50 +1100

    add implementation to remove an employee

--- client/src/components/Employee/Employee.tsx
+++ client/src/components/Employee/Employee.tsx
@@ -1,5 +1,17 @@
+import { deleteEmployee } from "../../services/employees";
+
 const Employee = ({ employee, added, setAdded }) => {
-	const { firstName, lastName, contractType, emailAddress } = employee;
+	const { firstName, lastName, contractType, emailAddress, id } = employee;
+
+	const handleClick = async () => {
+		try {
+				await deleteEmployee(id);
+				console.log("deleted");
+				setAdded(added + 1);
+		} catch (e) {
+				console.log(e.message);
+		}
+};
 
 	return(
 		<div>
