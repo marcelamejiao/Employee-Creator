322d5914fdd97b09a1246ee0af0891b9fa413cd1
Ruben Funai <ruben.funai@gmail.com>
2023-11-22 23:17:20 +1100

    feat: update employees server to load the api host via environment variable

--- client/src/services/employees.js
+++ client/src/services/employees.js
@@ -1,15 +1,18 @@
+// Load the API host from the environment if it exists
+const apiHost = import.meta.env.VITE_API_HOST ?? 'http://localhost:8080';
+
 export const getAllEmployees = async () => {
-    const response = await fetch("http://localhost:8080/employees");
+    const response = await fetch(`${apiHost}/employees`);
     return await response.json();
 };
 
 export const getEmployeeById = async (id) => {
-    const response = await fetch(`http://localhost:8080/employees/${id}`);
+    const response = await fetch(`${apiHost}/employees/${id}`);
     return await response.json();
 };
 
 export const createEmployee = async (data) => {
-    const response = await fetch("http://localhost:8080/employees", {
+    const response = await fetch(`${apiHost}/employees`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -23,7 +26,7 @@ export const createEmployee = async (data) => {
 };
 
 export const deleteEmployee = async (id) => {
-    const response = await fetch(`http://localhost:8080/employees/${id}`, {
+    const response = await fetch(`${apiHost}/employees/${id}`, {
         method: "DELETE",
     });
 
@@ -33,7 +36,7 @@ export const deleteEmployee = async (id) => {
 };
 
 export const updateEmployee = async (id, data) => {
-    const response =  await fetch(`http://localhost:8080/employees/${id}`, {
+    const response =  await fetch(`${apiHost}/employees/${id}`, {
         method: "PATCH",
         headers: {
             "Content-Type": "application/json",
