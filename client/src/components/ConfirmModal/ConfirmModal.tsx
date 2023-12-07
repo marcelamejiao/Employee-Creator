66e5cd305592358c14627d770fd78fc2b33c41f6
marcelamejiao <mejiao24@hotmail.com>
2023-12-07 17:30:12 +1100

    create confirm modal to use it when clicks the remove button

---
+++ client/src/components/ConfirmModal/ConfirmModal.tsx
@@ -0,0 +1,21 @@
+import styles from "./ConfirmModal.module.scss"
+
+type confirmModalProps = {
+	handleRemove(): void,
+	setShowModal(): void,
+};
+
+export const ConfirmModal = ({ handleRemove, setShowModal}: confirmModalProps) => {
+
+	return (
+		<>
+			<div className={styles.container}>
+				<h2>Are you sure you want to delete this employee?</h2>
+				<div className={styles.actions}>
+					<button className={styles.buttons} onClick={handleRemove}>Yes</button>
+					<button className={styles.buttons} onClick={setShowModal}>Cancel</button>
+				</div>
+			</div>
+		</>
+	)
+}
