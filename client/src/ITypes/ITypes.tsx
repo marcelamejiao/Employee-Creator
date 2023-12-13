0b0f8de7edd085fadbe800d53392d77d7fb56938
marcelamejiao <mejiao24@hotmail.com>
2023-12-13 16:35:41 +1100

    move Interface for types to a new file

---
+++ client/src/ITypes/ITypes.tsx
@@ -0,0 +1,15 @@
+interface ITypes {
+  firstName: string
+  lastName: string
+  middleName: string
+  emailAddress: string
+  mobileNumber: number
+  address: string
+  contractType: string
+  startDate: Date
+  finishDate: Date
+  contractBasis: string
+  hoursPerWeek: number
+}
+
+export default ITypes;
